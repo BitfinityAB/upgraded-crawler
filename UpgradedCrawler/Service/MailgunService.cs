87dced0aa4833105bc5359eebdd911118d01af42
Aziz Khakulov <aziz.khakulov@bitfinity.dev>
2026-04-20 10:05:17 +0200

    fix: restore mailgun_csharp to Core temporarily; fix MailgunService FluentAssertions usage

--- UpgradedCrawler/Service/MailgunService.cs
+++ UpgradedCrawler/Service/MailgunService.cs
@@ -1,44 +1,34 @@
-using FluentAssertions;
 using Mailgun.Messages;
 using Mailgun.Service;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json.Linq;
-using System.Net;
 using UpgradedCrawler.Core.Entities;
-using UpgradedCrawler.Core.Extensions;
 using UpgradedCrawler.Core.Interfaces;
+using UpgradedCrawler.Core.Extensions;
+
 namespace UpgradedCrawler.Service;
 
 public class MailgunService(IOptions<MailgunOptions> mailgunOptions) : IEmailService
 {
-    private readonly MailgunOptions mailgunOptions = mailgunOptions.Value;
+    private readonly MailgunOptions _mailgunOptions = mailgunOptions.Value;
 
     public async Task SendEmail(string fromAddress, string fromName, string to, string subject, ICollection<AssignmentAnnouncement> assignments)
     {
-        var mg = new MessageService(mailgunOptions.ApiKey, null, "api.eu.mailgun.net/v3");
+        var mg = new MessageService(_mailgunOptions.ApiKey, null, "api.eu.mailgun.net/v3");
 
         var message = new MessageBuilder()
-             .AddToRecipient(new Recipient
-             {
-                 Email = to
-             })
-             .SetSubject(subject)
-             .SetFromAddress(new Recipient { Email = fromAddress, DisplayName = fromName })
-             .SetTemplate(mailgunOptions.TemplateName, JObject.FromObject(GetMailgunTemplateData(assignments)))
-             .GetMessage();
+            .AddToRecipient(new Recipient { Email = to })
+            .SetSubject(subject)
+            .SetFromAddress(new Recipient { Email = fromAddress, DisplayName = fromName })
+            .SetTemplate(_mailgunOptions.TemplateName, JObject.FromObject(GetMailgunTemplateData(assignments)))
+            .GetMessage();
 
-        var content = await mg.SendMessageAsync(mailgunOptions.Domain, message);
-        content.Should().NotBeNull();
-        content.StatusCode.Should().Be(HttpStatusCode.OK);
+        var response = await mg.SendMessageAsync(_mailgunOptions.Domain, message);
+        if (response is null)
+            throw new InvalidOperationException("Mailgun SendMessageAsync returned null.");
+        response.EnsureSuccessStatusCode();
     }
 
-    private static MailgunTemplateData GetMailgunTemplateData(ICollection<AssignmentAnnouncement> assignments)
-    {
-        var data = new MailgunTemplateData
-        {
-            Assignments = assignments,
-            MultipleAssignments = assignments.Count > 1
-        };
-        return data;
-    }
+    private static MailgunTemplateData GetMailgunTemplateData(ICollection<AssignmentAnnouncement> assignments) =>
+        new() { Assignments = assignments, MultipleAssignments = assignments.Count > 1 };
 }
